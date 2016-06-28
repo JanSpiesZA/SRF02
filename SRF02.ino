7cad1f3d2a9e1a0eee96a9c1cabc8b6228e4c0a4
Jan <jan.spies@gmail.com>
2016-06-28 22:06:16 +0200

    feat: add serial input to get distance data only when asked

--- SRF02.ino
+++ SRF02.ino
@@ -1,76 +1,102 @@
-
+//First experiments in using the SRF02 with SERIAL comms
+//Allows distance measurements to be sent via Serial to PC
 
 #define CMD_REAL_RANGE_TX_cm      84
 #define CMD_REAL_RANGE_NO_TX_cm   81
+#define CMD_GET_RANGE             94
 
-int indata = 0;
-int adr = 1;
 int sendflag = 1;
 String readString;
+int oldTime = 0;
+String serialData = "";
+boolean done = false;
+int index = 0;
 
-int numSensors = 0;
-int sensorAddr[] = {0,1};     //Array used to store the addresses of the SRF02 sensors in the order in which they must be read
+int sensorAddr[] = {3,0,4,1,5,2,6};     //Array used to store the addresses of the SRF02 sensors in the order in which they must be read
+//sizeof give the amount of bytes used in the array and not the actual elements. When using int every element is 2 bytes long
+//  therefore the value must be divided by the siezeof(int) to get the total amount of elements in the array
+const int numSensors = sizeof(sensorAddr)/ sizeof(int);    
+int sensorDist[numSensors];
 
-void setup() {
-        Serial.begin(9600);        
-        Serial3.begin(9600);  //SRF modules
-        
-        Serial.println("Started");
-        //sizeof give the amount of bytes used in the array and not the actual elements. When using int every element is 2 bytes long
-        //  therefore the value must be divided by the siezeof(int) to get the total amount of elements in the array
-        numSensors = sizeof(sensorAddr)/ sizeof(int);    
-        Serial.println(numSensors);        
-        delay(1000);
+void setup() 
+{
+  Serial.begin(115200);        
+  Serial3.begin(9600);  //Comms port for SRF modules        
+  Serial.println("US Interface Started");
+  delay(1000);
 }
 
 void loop()
-{ 
-  if (sendflag == 1)
-  {        
-    for (int n = 0; n <= numSensors-1; n++)
-    { 
-      Serial.print("Sensor adr: ");
-      Serial.println(sensorAddr[n]);
-      Serial3.write(sensorAddr[n]);
-      Serial3.write(CMD_REAL_RANGE_TX_cm);
-      Serial.println("Send");
-
-//      while (2 >= Serial3.available())
-//      {
-//       indata = Serial3.read()<<8;
-//       indata |= Serial3.read();
-//       Serial.println(indata); 
-//      }
+{   
+  //Lees seriedata tot 'n EOL karakter gekry word
+  if (Serial.available() >0)
+  {
+    char c = Serial.read();
+    
+    switch(c)
+    {
+      case '<':      //new command line starts
+      index = 0;
+      break;
+      
+      case '\r':
+        done = true;        
+        break;
+        
+      default:
+        serialData += c;        //Add serial data input char to string
+        break;
     }
-    sendflag = 0;
+  }   
+
+  if (done)
+  {
+    done = false;
+    char testChar = serialData.charAt(0);
+
+    switch(testChar)
+    {
+      case 's':
+      {
+        for (int n = 0; n <= numSensors-1; n++)
+        { 
+          sensorDist[n] = getRange(sensorAddr[n]);  
+          Serial.print(sensorAddr[n]);
+          Serial.print(':');
+          Serial.print(sensorDist[n]);  
+          Serial.print('\t');    
+          delay(5);
+        }
+        Serial.println();
+        break;
+      }
+    }
+    serialData = "";
   }
-  delay(500);        
+
+  
+  //Ping distance to nearest object for each of the sensors
+  for (int n = 0; n <= numSensors-1; n++)
+  { 
+    ping(sensorAddr[n]);
+    delay(10);
+  }
 }
 
 //Ping a sensors without sending the distance data back
 void ping(int _sensorAddr)
 {
-   Serial.print("PING Sensor with adr: #");
-   Serial.println(_sensorAddr);
    Serial3.write(_sensorAddr);
    Serial3.write(CMD_REAL_RANGE_NO_TX_cm);
 }
 
 int getRange(int _sensorAddr)
 {
+  Serial3.write(_sensorAddr);
+  Serial3.write(CMD_GET_RANGE);
+
+  while(Serial3.available() < 2);     //Wait until 2 bytes received
   int rxData = Serial3.read()<<8;
   rxData |= Serial3.read();
   return rxData;
-}
-
-void serialEvent3()
-{
-  if (Serial3.available() >= 2)
-  {
-    indata = Serial3.read()<<8;
-    indata |= Serial3.read();
-    Serial.println(indata);
-    sendflag = 1;
-    delay(1000);    
-  }    
 }
